96bb2f79ea36b5f92413b07f6621847ef373541d
Waut Schepens <133114632+waut10000@users.noreply.github.com>
2023-11-07 09:36:40 +0100

    Create security.h
    Security file for sensitive information

---
+++ security.h
@@ -0,0 +1,11 @@
+//WiFi connections
+const char* ssid[] = {"SSID0", "SSID1", "SSID2"};
+const char* passwords[] = {"Password0", "Password1", "Password2"};
+
+// Server URL and endpoint
+const char* serverUrl = "http://..."; //url naar database
+const char* username = "username"; // database username
+const char* passwordDB = "password"; // db username password
+
+// uuid van vuilnisbak
+String vuilnisbak_id = "trashcan uuid"; 
